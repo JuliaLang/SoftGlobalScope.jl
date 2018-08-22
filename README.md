dc1bfd632f8a2c3c32a24fdafee31c53f00e3d6c
Steven G. Johnson <stevenj@alum.mit.edu>
2018-08-22 12:17:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# SoftGlobalScope
+
+[![Build Status](https://travis-ci.org/stevengj/SoftGlobalScope.jl.svg?branch=master)](https://travis-ci.org/stevengj/SoftGlobalScope.jl)
+
+SoftGlobalScope is a package for the [Julia language](http://julialang.org/) that simplifies the [variable scoping rules](https://docs.julialang.org/en/stable/manual/variables-and-scoping/) for code in *global* scope.   It is intended for interactive shells (the REPL, [IJulia](https://github.com/JuliaLang/IJulia.jl), etcetera) to make it easier to work interactively with Julia, especially for beginners.
+
+In particular, SoftGlobalScope provides a function `softscope` that can transform Julia code from using the default "hard" scoping rules to simpler "soft" scoping rules in global scope only.
+
+## Hard and soft global scopes
+
+[Starting in Julia 0.7](https://github.com/JuliaLang/julia/pull/19324), when you *assign to* global variables in the context of an inner scope (a `for` loop or a `let` statement) you need to explicitly declare the variable
+as `global` in order to distinguish it from declaring a new variable.  We refer to this as "hard" scoping rules.  For example, the following code gives an warning in 0.7:
+
+```jl
+julia> s = 0
+0
+
+julia> for i = 1:10
+           s = s + i
+       end
+┌ Warning: Deprecated syntax `implicit assignment to global variable `s``.
+└ Use `global s` instead.
+```
+
+and an error in Julia 1.0:
+
+```jl
+julia> s = 0
+0
+
+julia> for i = 1:10
+           s = s + i   # wrong: defines a new local variable s
+       end
+ERROR: UndefVarError: s not defined
+```
+
+To make it work in 1.0, you need a `global` declaration:
+```
+julia> for i = 1:10
+           global s = s + i
+       end
+
+julia> s      # should be 1 + 2 + ⋯ + 10 = 55
+55
+```
+
+This only applies to *global* variables; similar code *inside a function* (or whenever `s` is a *local* variable) works fine without any added keyword:
+```
+julia> function f(n)
+           s = 0
+           for i = 1:n
+               s = s + i
+           end
+           return s
+       end
+f (generic function with 1 method)
+
+julia> f(10)
+55
+```
+
+There were [various reasons](https://github.com/JuliaLang/julia/pull/19324) for this scoping rule, e.g. to facilitate [static analysis](https://en.wikipedia.org/wiki/Static_program_analysis) by the compiler, and it isn't too onerous in "serious" Julia code where [little code executes in global scope](https://docs.julialang.org/en/stable/manual/performance-tips/#Avoid-global-variables-1).
+
+However, for *interactive* use, especially for new users, the necessity of the `global` keyword, and the difference between code in local and global scopes, [can be confusing](https://github.com/JuliaLang/julia/issues/28789#issuecomment-414930024).   The SoftGlobalScope package exists to make it easier for *interactive shells* to automatically insert the `global` keyword in common cases, what we term "soft" global scope.
+
+## Usage
+
+The `SoftGlobalScope` module exports two functions `softscope` and `softscope_include_string`:
+
+You can transform the expression using `softscope(module, expression)` to automatically insert the necessary `global` keyword:
+```jl
+julia> softscope(Main, :(for i = 1:10
+           s += i
+       end))
+:(for i = 1:10
+      #= REPL[3]:2 =#
+      global s += i
+  end)
+```
+You can then execute the statement with `eval`.  Alternatively, you can execute an entire sequence of statements
+using "soft" global scoping rules via `softscope_include_string(module, string, filename="string")`:
+```jl
+julia> softscope_include_string(Main, """
+       s = 0
+       for i = 1:10
+           s += i
+       end
+       s
+       """)
+55
+```
+(This function works like `include_string`, returning the value of the last evaluated expression.)
+
+## Contact
+
+SoftGlobalScope was written by [Steven G. Johnson](http://math.mit.edu/~stevenj/) and is free/open-source software under the [MIT/Expat license](LICENSE.md).  Please file bug reports and feature requests at the [SoftGlobalScope github page](https://github.com/stevengj/SoftGlobalScope.jl).
---
+++ src/SoftGlobalScope.jl
@@ -0,0 +1,105 @@
+"""
+SoftGlobalScope is a package that simplifies the [variable scoping rules](https://docs.julialang.org/en/stable/manual/variables-and-scoping/)
+for code in *global* scope.   It is intended for interactive shells (the REPL, [IJulia](https://github.com/JuliaLang/IJulia.jl),
+etcetera) to make it easier to work interactively with Julia, especially for beginners.
+
+In particular, SoftGlobalScope provides a function `softscope` that can transform Julia code from using the default
+"hard" (local) scoping rules to simpler "soft" scoping rules in global scope, and a function `softscope_include_string`
+that can evaluate a whole string (similar to `include_string`) using these rules.
+
+For example, if `s` is a global variable in the current module (e.g. `Main`), the following code is an error in
+Julia 1.0:
+```
+for i = 1:10
+    s += i     # declares a new local variable `s`
+end
+```
+Instead, you can transform the expression using `softscope` to automatically insert the necessary `global` keyword:
+```jl
+julia> softscope(Main, :(for i = 1:10
+           s += i
+       end))
+:(for i = 1:10
+      #= REPL[3]:2 =#
+      global s += i
+  end)
+```
+You can then execute the statement with `eval`.  Alternatively, you can execute an entire sequence of statements
+using "soft" global scoping rules via `softscope_include_string`:
+```jl
+julia> softscope_include_string(Main, """
+       s = 0
+       for i = 1:10
+           s += i
+       end
+       s
+       """)
+55
+```
+(This function works like `include_string`, returning the value of the last evaluated expression.)
+"""
+module SoftGlobalScope
+export softscope, softscope_include_string
+
+using Base.Meta: isexpr
+
+const assignments = Set((:(=), :(+=), :(-=), :(*=), :(/=), :(//=), :(\=), :(^=), :(÷=), :(%=), :(<<=), :(>>=), :(>>>=), :(|=), :(&=), :(⊻=), :($=)))
+
+# extract the local variable name (e.g. `x`) from an assignment expression (e.g. `x=1`)
+localvar(ex::Expr) = isexpr(ex, :(=)) ? ex.args[1] : nothing
+localvar(ex) = nothing
+
+# Transform expression `ex` to "soft" scoping rules, where `globals` is a collection
+# (e.g. `Set`) of global-variable symbols to implicitly qualify with `global`, and
+# `insertglobal` is whether to insert the `global` keyword at the top level of
+# `ex`.  (Usually, you pass `insertglobal=false` to start with and then it is
+# recursively set to `true` for local scopes introduced by `for` etcetera.)
+function _softscope(ex::Expr, globals, insertglobal::Bool=false)
+    if isexpr(ex, :for) || isexpr(ex, :while)
+        return Expr(ex.head, ex.args[1], _softscope(ex.args[2], globals, true))
+    elseif isexpr(ex, :try)
+        try_clause = _softscope(ex.args[1], globals, true)
+        catch_clause = _softscope(ex.args[1], ex.args[2] isa Symbol ? setdiff(globals, ex.args[2:2]) : globals, true)
+        finally_clause = _softscope(ex.args[4], globals, true)
+        return Expr(:try, try_clause, ex.args[2], catch_clause, finally_clause)
+    elseif isexpr(ex, :let)
+        letglobals = setdiff(globals, isexpr(ex.args[1], :(=)) ? [ex.args[1].args[1]] : [localvar(ex) for ex in ex.args[1].args])
+        return Expr(ex.head, _softscope(ex.args[1], globals, insertglobal),
+                             _softscope(ex.args[2], letglobals, true))
+    elseif isexpr(ex, :block) || isexpr(ex, :if)
+        return Expr(ex.head, _softscope.(ex.args, Ref(globals), insertglobal)...)
+    elseif insertglobal && ex.head in assignments && ex.args[1] in globals
+        return Expr(:global, Expr(ex.head, ex.args[1], _softscope(ex.args[2], globals, insertglobal)))
+    else
+        return ex
+    end
+end
+_softscope(ex, globals, insertglobal::Bool=false) = ex
+
+"""
+    softscope(m::Module, ast)
+
+Transform the abstract syntax tree `ast` (a quoted Julia expression) to use "soft"
+scoping rules for the global variables defined in `m`, returning the new expression.
+"""
+softscope(m::Module, ast) = _softscope(ast, Set(names(m)))
+
+"""
+    softscope_include_string(m::Module, code::AbstractString, filename::AbstractString="string")
+
+Like [`include_string`](@ref), but evaluates `code` using "soft"
+scoping rules for the global variables defined in `m`.
+"""
+function softscope_include_string(m::Module, code::AbstractString, filename::AbstractString="string")
+    # use the undocumented parse_input_line function so that we preserve
+    # the filename and line-number information.
+    expr = Base.parse_input_line("begin; "*code*"\nend\n", filename=filename)
+    retval = nothing
+    # expr.args should consist of LineNumberNodes followed by expressions to evaluate
+    for i = 2:2:length(expr.args)
+        retval = Core.eval(m, softscope(m, Expr(:block, expr.args[i-1:i]...)))
+    end
+    return retval
+end
+
+end # module
