76a8ab3a2daaa48c20c15032d9367af535827424
David AW Barton <david.barton@bristol.ac.uk>
2018-08-31 16:54:21 +0100

    Another test

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,6 +46,7 @@ end
         @test softscope(TestMod, nl"for i = 1:10; let a = i ; println(a) ; end ; end") == nl"for i = 1:10; let a = i ; println(a) ; end ; end"
         @test softscope(TestMod, nl"begin; local x; x = 0; for i = 1:10; x += i ; end ; end") == nl"begin; local x; x = 0; for i = 1:10; x += i ; end ; end"
         @test softscope(TestMod, nl"let x=1; x=0; for i = 1:10; x += i; end; end") == nl"let x=1; x=0; for i = 1:10; x += i; end; end"
+        @test softscope(TestMod, nl"for i = 1:10; for j = 1:3; global x += 1; end; x = 3; end") == nl"for i = 1:10; for j = 1:3; global x += 1; end; x = 3; end"
     end
 end
 
